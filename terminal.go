8cb769c1a5721ffa9ba5e820a2dad7b343a1076a
Calum Price <calumprice79@gmail.com>
2025-01-30 19:08:15 +0000

    changed time format displayed in har log table

--- terminal.go
+++ terminal.go
@@ -191,7 +191,7 @@ func (t *Terminal) populateRow(rowIndex int, entry Entry) {
 	t.setTableCell(rowIndex, 3, formatUrl(entry.Request.URL), tview.AlignLeft, true)
 	t.setTableCell(rowIndex, 4, entry.ServerIP, tview.AlignCenter, true)
 	t.setTableCell(rowIndex, 5, entry.Connection, tview.AlignCenter, true)
-	t.setTableCell(rowIndex, 6, fmt.Sprintf("%.2f", entry.Time*1000), tview.AlignRight, true)
+	t.setTableCell(rowIndex, 6, fmt.Sprintf("%.2f", entry.Time), tview.AlignRight, true)
 }
 
 // Helper function to set table cells
